b45dd962b4a5452fcf86d28f8d5cf0e6abba57e8
Reinaldo <reinaldo.robles@icloud.com>
2023-06-21 20:20:57 -0400

    modified:   index.html modified:   style.css modified:   script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 import {menuArray} from './data.js'
-// const modal = document.getElementById('modal')
+const order = document.getElementById("order")
+const newObj = document.getElementById('new-obj')
+const modal = document.getElementById('modal')
+const payBtn = document.getElementById('pay-btn')
 const purchaseBtn = document.getElementById('purchase-btn')
 let orderArray = []
 
-// purchaseBtn.addEventListener('click', (e)=> {
-//    modal.classList.remove('hidden')
-// })
+purchaseBtn.addEventListener('click', (e)=> {
+   modal.classList.remove('hidden')
+   newObj.classList.add('hidden')
+   order.classList.add('hidden')
+})
+
+payBtn.addEventListener('click', () => {
+  modal.classList.add('hidden')
+})
 
 document.addEventListener('click', (e) => {
     if(e.target.dataset.add) {
@@ -14,12 +23,10 @@ document.addEventListener('click', (e) => {
     } 
      else if (e.target.dataset.remove){
       orderArray.splice(e.target.dataset.remove, 1)
-      document.getElementById('order').innerHTML = `<h3 class="order-title">Your order</h3> ${renderOrder(orderArray)}`  
+      order.innerHTML = `<h3 class="order-title">Your order</h3> ${renderOrder(orderArray)}`  
       getTotal(orderArray)  
     } 
-     else if (e.target.dataset.modal) {
-      purchaseBtn(e.target.dataset.modal)       
-     }
+     
 })
 
 function getTotal(items) {
@@ -34,10 +41,10 @@ function handleClick(dataId) {
     for (let item of menuArray) {
       if(item.id === numId) {
         orderArray.push(item)
-        document.getElementById('total-price').classList.remove('hidden')
+        newObj.classList.remove('hidden')
       }
     }
-  document.getElementById('order').innerHTML = `<h3 class="order-title">Your order</h3> ${renderOrder(orderArray)}`
+  order.innerHTML = `<h3 class="order-title">Your order</h3> ${renderOrder(orderArray)}`
  }
 
 function renderOrder(orderArray) {
@@ -60,16 +67,16 @@ function menu() {
     menuArray.forEach(menu => {
   newMenu += `
     <div class="menu-items">
-             <spam class="emoji">${menu.emoji}</spam>
-             <div class="names">
+               <spam class="emoji">${menu.emoji}</spam>
+            <div class="names">
                <p class="name">${menu.name}</p>
                <p class="ing">${menu.ingredients.join(', ')}</p>
                <p class="price">$${menu.price}</p>
-              </div>
-               <i class="fa-regular fa-plus" data-add="${menu.id}"></i>
-              </div>
-              <hr/>
-              `
+            </div>
+        <i class="fa-regular fa-plus" data-add="${menu.id}"></i>
+    </div>
+     <hr/>
+           `
             })
             return newMenu
           }
@@ -78,6 +85,6 @@ function menu() {
             document.getElementById('menu').innerHTML = menu()
           }
           render()
-          // <div class="div-btn"></div> / este div estaba agrupando al plus icon  testing.......
+
 
 
